bae7b3349e468c647ccb1b444b3e84634c678ca9
robsca <scalas.roberto@gmail.com>
2022-10-12 19:15:30 +0100

    Implemented in streamlit

--- make_more/make_more.py
+++ make_more/make_more.py
@@ -1,58 +1,71 @@
-# open txt file
-words = open('names.txt', 'r').read().splitlines() # readlines() returns a list of lines
-
-# print the first 10 words
-for word in words[:10]:
-    print(word)
-
-# Check the lenght of the list and print it
-print('Length of the list: ', len(words))
-
-# Check and print the shortest word
-shortest = min(words, key=len)
-print('Shortest word: ', shortest, len(shortest))
-
-# Check and print the longest word
-longest = max(words, key=len)
-print('Longest word: ', longest, len(longest))
-
-
+import streamlit as st
+st.set_page_config(layout="wide")
+import pandas as pd
 import torch
-N = torch.zeros((27,27), dtype=torch.int32)
 
-list_of_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.']
-string_to_integer = {letter: i for i, letter in enumerate(list_of_letters)}
-integer_to_string = {i: letter for i, letter in enumerate(list_of_letters)}
-
-print(integer_to_string)
-
-for w in words:
-    # w = single name
-    # add . to the end of the name
-    w = '.' + w + '.'
-    for i in range(len(w)-1):
-        ch_1 = w[i]
-        ch_2 = w[i+1]
-        N[string_to_integer[ch_1], string_to_integer[ch_2]] += 1
-
-# plot N as matrix
-import matplotlib.pyplot as plt
-plt.imshow(N)
-plt.show()
+st.title('Make More')
 
 
-# repeat until you get a .
+with st.sidebar.expander('Import data'):
+    uploaded_file_1 = st.file_uploader('names')
+    
+if uploaded_file_1 is not None:
+    #read txt file
+    words = uploaded_file_1.read().decode('utf-8').splitlines()
+    # transform in a dataframe
+    df = pd.DataFrame(words, columns=['words'])
+    st.write(df)
+    '''
+    Create a 27x27 matrix of zeros,
+    append the count value for every couple of letters,
+    representing the real distribution of the next letter, as probabilities.
+    '''
+    # 1. Create a 27x27 matrix of zeros
+    import torch
+    N = torch.zeros((27,27), dtype=torch.int32)
 
-import random
-def generate_name():
-    name = ''
-    ch = integer_to_string[random.randint(0, 26)] # start with a random letter
-    name += ch
-    while ch != '.':
-        # then sample the next letter from the distribution of the next letter
-        ch = integer_to_string[torch.multinomial(N[string_to_integer[ch]].float(), 1).item()]
+    list_of_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.']
+    string_to_integer = {letter: i for i, letter in enumerate(list_of_letters)}
+    integer_to_string = {i: letter for i, letter in enumerate(list_of_letters)}
+
+    for w in words:
+        w = '.' + w + '.' # add . to the end of the name
+        for i in range(len(w)-1):
+            ch_1 = w[i] # current letter as string
+            ch_2 = w[i+1] # next letter as string
+            row = string_to_integer[ch_1] # current letter as integer
+            col = string_to_integer[ch_2] # next letter as integer
+            N[row, col] += 1
+
+    # 2. Plot N as matrix
+    import plotly.express as px
+    fig = px.imshow(N, color_continuous_scale='Blues')
+    # add labels
+    fig.update_xaxes(title_text='Next letter')
+    fig.update_yaxes(title_text='Current letter')
+    # add text to each cell
+    for i in range(27):
+        for j in range(27):
+            xh1 = integer_to_string[i]
+            xh2 = integer_to_string[j]
+            fig.add_annotation(text=xh1 + xh2, x=i, y=j, showarrow=False)
+    # set size
+    fig.update_layout(width=800, height=800)
+     
+    st.plotly_chart(fig, use_container_width=True)
+
+    # 3. Create function to generate a name
+    import random
+    def generate_name():
+        name = ''
+        ch = integer_to_string[random.randint(0, 26)] # start with a random letter
         name += ch
-    return name
+        while ch != '.':
+            # then sample the next letter from the distribution of the next letter
+            ch = integer_to_string[torch.multinomial(N[string_to_integer[ch]].float(), 1).item()]
+            name += ch
+        return name
 
-for i in range(10):
-    print(generate_name())
+    # test the code generating 10 names
+    for i in range(10):
+        print(generate_name())
